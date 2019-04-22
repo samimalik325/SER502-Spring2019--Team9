170a7d73958d076a8d6902ae00677a1d21f10fc6
Sami Malik <gamyinteractive@Sami-Malik.local>
2019-04-21 17:41:34 -0700

    Added intermediate code generator file which will be used for the generation of intermediate code from code samples

---
+++ src/com/simple/compiler/SimpleIntermediateCodeGenarator.java
@@ -0,0 +1,17 @@
+package com.simple.compiler;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+//import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+//import org.antlr.v4.runtime.tree.TerminalNode;
+
+
+import com.simple.common.SimpleConstants;
+
+public class SimpleIntermediateCodeGenarator extends simpleBaseListener  {
+	
+	
+}
+	
