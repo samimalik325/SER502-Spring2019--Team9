a556e3bf8e46468c2a39159fad307eb8eff3451f
Sami Malik <gamyinteractive@Sami-Malik.local>
2019-04-21 19:26:05 -0700

    implemented intermediate file reaading for execution

--- src/com/simple/runtime/SimpleExecuter.java
+++ src/com/simple/runtime/SimpleExecuter.java
@@ -12,6 +12,32 @@ import com.simple.common.SimpleConstants;
 
 public class SimpleExecuter {
 	
+	public static void main(String[] args) {
+//		String filename = "";
+//		if (args.length > 0){
+//	    		filename = args[0];
+//	    	} else {
+//			System.out.println("input filename can't be empty");
+//			return;
+//		}
+//			
+//		if (!filename.contains(".simple")) {
+//			System.out.println("Enter a valid .simple file");
+//			return;
+//		}
+		
+		try {
+			for (String line : Files.readAllLines(Paths.get("./resources/SamplePrograms/test.simpleint")) {
+				listofCode.add(line);
+			}
+		} catch (IOException e) {
+			System.out.println("File not found...Please specify valid input file");
+			e.printStackTrace();
+		}
+		
+
+	}
+	
 	
 	
 }
