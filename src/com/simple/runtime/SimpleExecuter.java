54813e5e5ed8ee8729ff8ce9fb53b09f55ccdcd1
Sami Malik <gamyinteractive@Sami-Malik.local>
2019-04-21 17:42:45 -0700

    added executer file which will be used to execute the code using intermediate code file

---
+++ src/com/simple/runtime/SimpleExecuter.java
@@ -0,0 +1,17 @@
+package com.simple.runtime;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+//import java.util.Scanner;
+import java.util.Stack;
+
+import com.simple.common.SimpleConstants;
+
+public class SimpleExecuter {
+	
+	
+	
+}
