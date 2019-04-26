97bfb0a44773aa2eebeba1cb9e38896100bd2842
Sami Malik <gamyinteractive@sami-malik.3500.dhcp.asu.edu>
2019-04-26 16:37:27 -0700

    created execution method in simpleexecuter for execution of intermediate code

--- src/com/simple/runtime/SimpleExecuter.java
+++ src/com/simple/runtime/SimpleExecuter.java
@@ -12,6 +12,50 @@ import com.simple.common.SimpleConstants;
 
 public class SimpleExecuter {
 	
+
+	static ArrayList<String> listofCode = new ArrayList<String>();
+	
+	static int indexForIteration = 0;
+
+	static Stack<Boolean> stackForBooleans = new Stack<Boolean>();
+	
+	
+	public static void main(String[] args) {
+		String filename = "resources/SamplePrograms/function.simpleint";
+		/*if (args.length > 0){
+	    		filename = args[0];
+	    	} else {
+			System.out.println("input filename can't be empty");
+			return;
+		}*/
+			
+		if (!filename.contains(".simpleint")) {
+//			System.out.println("Enter a valid .simple file");
+			return;
+		}
+		
+		try {
+			for (String line : Files.readAllLines(Paths.get(filename))) {
+				listofCode.add(line);
+			}
+		} catch (IOException e) {
+//			System.out.println("File not found...Please specify valid input file");
+			e.printStackTrace();
+		}
+		
+		for (indexForIteration=0; indexForIteration<listofCode.size(); indexForIteration++) {
+			
+			execute();
+		}
+	}
+	
+	static public void execute(){
+		
+		
+		System.out.println("In Execute File");
+		
+	}
+	
 	
 	
 }
